e0d1da22a9cc44ff931f4cfac418a9d877084e13
Jeremy2023812 <82452631+Jeremy2023812@users.noreply.github.com>
2021-05-20 23:00:46 -0500

    Create FuncionPrimitivaEj13_IrvingJeremyMartinezAvila_3202.m

---
+++ FuncionPrimitivaEj13_IrvingJeremyMartinezAvila_3202.m
@@ -0,0 +1,13 @@
+% Titulo: Ejercicio numero 13
+% Descripcion: Script para desarrrollar el Ejericio13
+% Autor: Irving Jeremy Martinez Avila @Neo
+% Fecha: 19 de mayo del 2021
+
+
+%paqueteria symbolic.
+pkg load symbolic
+%Dame un valor inicial para la variable.
+syms x;
+%Resultado del ejercicio es:
+int (3*x^4) 
+ans = (sym)
