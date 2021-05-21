d73e269cf7d3965e96802a3cb3038888eb30f3b0
Jeremy2023812 <82452631+Jeremy2023812@users.noreply.github.com>
2021-05-20 22:43:42 -0500

    Create FuncionPrimitivaEj08_IrvingJeremyMartinezAvila_3202.m

---
+++ FuncionPrimitivaEj08_IrvingJeremyMartinezAvila_3202.m
@@ -0,0 +1,15 @@
+% Titulo: Ejercicio numero 8
+% Descripcion: Script para desarrrollar el Ejericio8
+% Autor: Irving Jeremy Martinez Avila @Neo
+% Fecha: 19 de mayo del 2021
+
+%paqueteria symbolic.
+pkg load symbolic
+%Dame un valor inicial para la variable.
+syms x;
+%Resultado del ejercicio es:
+int(x^-7)
+ans = (sym)
+
+%Dame un valor inicial para la variable.
+%Resultado del ejercicio es:
