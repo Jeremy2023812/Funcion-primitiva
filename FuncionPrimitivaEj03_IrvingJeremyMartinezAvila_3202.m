7b7e56677e677e0d4c329b2e302d16e63f33a5cc
Jeremy2023812 <82452631+Jeremy2023812@users.noreply.github.com>
2021-05-20 22:23:16 -0500

    Create FuncionPrimitivaEj03_IrvingJeremyMartinezAvila_3202.m

---
+++ FuncionPrimitivaEj03_IrvingJeremyMartinezAvila_3202.m
@@ -0,0 +1,12 @@
+% Titulo: Ejercicio Numero 3
+% Descripcion: Script para desarrrollar el Ejericio3
+% Autor: Irving Jeremy Martinez Avila @Neo
+% Fecha: 19 de mayo del 2021
+
+%paqueteria symbolic
+pkg load symbolic
+%Dame un valor inicial para la variable.
+syms x;
+%Resultado del ejercicio es: 
+int (5,x)
+ans = (sym)
