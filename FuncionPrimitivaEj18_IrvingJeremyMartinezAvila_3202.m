15c6e561e92cb6c87f9b9188eff1abb1e22b9d6b
Jeremy2023812 <82452631+Jeremy2023812@users.noreply.github.com>
2021-05-20 23:17:28 -0500

    Create FuncionPrimitivaEj18_IrvingJeremyMartinezAvila_3202.m

---
+++ FuncionPrimitivaEj18_IrvingJeremyMartinezAvila_3202.m
@@ -0,0 +1,13 @@
+% Titulo: Ejercicio numero 18
+% Descripcion: Script para desarrrollar el Ejericio18
+% Autor: Irving Jeremy Martinez Avila @Neo
+% Fecha: 19 de mayo del 2021
+
+
+%paqueteria symbolic.
+pkg load symbolic
+%Dame un valor inicial para la variable.
+syms x;
+%Resultado del ejercicio es:
+int (1./x.^3) 
+ans = (sym)
