4d12d0874630f91362754c750e128ee2f4dc7776
Jeremy2023812 <82452631+Jeremy2023812@users.noreply.github.com>
2021-05-20 22:47:54 -0500

    Create FuncionPrimitivaEj10_IrvingJeremyMartinezAvila_3202.m

---
+++ FuncionPrimitivaEj10_IrvingJeremyMartinezAvila_3202.m
@@ -0,0 +1,13 @@
+% Titulo: Ejercicio numero 10
+% Descripcion: Script para desarrrollar el Ejericio10
+% Autor: Irving Jeremy Martinez Avila @Neo
+% Fecha: 19 de mayo del 2021
+
+
+%paqueteria symbolic.
+pkg load symbolic
+%Dame un valor inicial para la variable.
+syms x;
+%Resultado del ejercicio es:
+int(x^(3./2))
+ans = (sym)
