80d2a4bc926619aa0684ac108cec6212e5a3d806
Jeremy2023812 <82452631+Jeremy2023812@users.noreply.github.com>
2021-05-20 22:11:47 -0500

    Create FuncionPrimitivaEj02_IrvingJeremyMartinezAvila_3202.m

---
+++ FuncionPrimitivaEj02_IrvingJeremyMartinezAvila_3202.m
@@ -0,0 +1,12 @@
+% Titulo: Ejercicio numero 2
+% Descripcion: Script para desarrrollar el Ejericio2
+% Autor: Irving Jeremy Martinez Avila @Neo
+% Fecha: 19 de mayo del 2021
+
+%paqueteria symbolic
+pkg load symbolic
+%Dame un valor inicial para la variable.
+syms x;
+%Resultado de la funcion. 
+int(x.^3)
+ans = (sym) 
