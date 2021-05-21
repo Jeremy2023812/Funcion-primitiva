a6b59764ba09403632058a4a23554774741e0c4f
Jeremy2023812 <82452631+Jeremy2023812@users.noreply.github.com>
2021-05-20 22:50:35 -0500

    Create FuncionPrimitivaEj11_IrvingJeremyMartinezAvila_3202.m

---
+++ FuncionPrimitivaEj11_IrvingJeremyMartinezAvila_3202.m
@@ -0,0 +1,13 @@
+% Titulo: Ejercicio numero 11
+% Descripcion: Script para desarrrollar el Ejericio11
+% Autor: Irving Jeremy Martinez Avila @Neo
+% Fecha: 19 de mayo del 2021
+
+
+%paqueteria symbolic.
+pkg load symbolic
+%Dame un valor inicial para la variable.
+syms x;
+%Resultado del ejercicio es:
+int (sqrt(x)) 
+ans = (sym)
