bb527e4b11ea7ce136a3710179642b335c849230
Jeremy2023812 <82452631+Jeremy2023812@users.noreply.github.com>
2021-05-20 23:12:15 -0500

    Create FuncionPrimitivaEj17_IrvingJeremyMartinezAvila_3202.m

---
+++ FuncionPrimitivaEj17_IrvingJeremyMartinezAvila_3202.m
@@ -0,0 +1,13 @@
+% Titulo: Ejercicio numero 17
+% Descripcion: Script para desarrrollar el Ejericio17
+% Autor: Irving Jeremy Martinez Avila @Neo
+% Fecha: 19 de mayo del 2021
+
+
+%paqueteria symbolic.
+pkg load symbolic
+%Dame un valor inicial para la variable.
+syms u;
+%Resultado del ejercicio es:
+int (3*u^5-2*u^3) 
+ans = (sym)
