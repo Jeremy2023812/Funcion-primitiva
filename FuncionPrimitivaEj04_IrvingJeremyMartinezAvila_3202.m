b080192cddd5ceaebc2a3ce867a0ccad4e453e28
Jeremy2023812 <82452631+Jeremy2023812@users.noreply.github.com>
2021-05-20 22:28:53 -0500

    Create FuncionPrimitivaEj04_IrvingJeremyMartinezAvila_3202.m

---
+++ FuncionPrimitivaEj04_IrvingJeremyMartinezAvila_3202.m
@@ -0,0 +1,13 @@
+% Titulo: Ejercicio numero 4
+% Descripcion: Script para desarrrollar el Ejericio3
+% Autor: Irving Jeremy Martinez Avila @Neo
+% Fecha: 19 de mayo del 2021
+
+
+%paqueteria symbolic
+pkg load symbolic
+%Dame un valor inicial para la variable.
+syms x;
+%Resultado del ejercicio es: 
+int(8*x^2)-(3*x^2)
+ans = (sym)
