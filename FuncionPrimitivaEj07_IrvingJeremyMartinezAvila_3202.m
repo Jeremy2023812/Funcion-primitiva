94760dfb4d3899435afc2fefb6e08d9be1e33c5d
Jeremy2023812 <82452631+Jeremy2023812@users.noreply.github.com>
2021-05-20 22:40:28 -0500

    Create FuncionPrimitivaEj07_IrvingJeremyMartinezAvila_3202.m

---
+++ FuncionPrimitivaEj07_IrvingJeremyMartinezAvila_3202.m
@@ -0,0 +1,13 @@
+% Titulo: Ejercicio numero 7
+% Descripcion: Script para desarrrollar el Ejericio7
+% Autor: Irving Jeremy Martinez Avila @Neo
+% Fecha: 19 de mayo del 2021
+
+
+%paqueteria symbolic
+pkg load symbolic
+%Dame un valor inicial para la variable.
+syms x;
+%Resultado del ejercicio es:  
+int(x.^5)
+ans = (sym)
