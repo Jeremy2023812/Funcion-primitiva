2899de0789659b5d16f0689b817db50061233e5b
Jeremy2023812 <82452631+Jeremy2023812@users.noreply.github.com>
2021-05-20 23:07:03 -0500

    Create FuncionPrimitivaEj15_IrvingJeremyMartinezAvila_3202.m

---
+++ FuncionPrimitivaEj15_IrvingJeremyMartinezAvila_3202.m
@@ -0,0 +1,13 @@
+% Titulo: Ejercicio numero 15
+% Descripcion: Script para desarrrollar el Ejericio15
+% Autor: Irving Jeremy Martinez Avila @Neo
+% Fecha: 19 de mayo del 2021
+
+
+%Paqueteria symbolic.
+pkg load symbolic
+%Dame un valor inicial para la variable.
+syms t;
+%Resultado del ejercicio es:
+int (3./t.^5)
+ans = (sym)
